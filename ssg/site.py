f3244bbd68e6ada3c0c9d60bdddd58698f6b4e3c
Johan Van Hoye <johan.vanhoye@wolterskluwer.com>
2022-05-10 23:14:22 +0200

    Module1 Fix 1 -- indentation error

--- ssg/site.py
+++ ssg/site.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 class Site:
 	def __init__(self, source, dest):
-	self.source = Path(source)
-	self.dest = Path(dest)
+	    self.source = Path(source)
+	    self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = f"{self.dest}/{path.relative_to(self.source)}"

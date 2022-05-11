9dd7d3ce62598c8b3b19c6d6038f5f3a26bc005f
Johan Van Hoye <johan.vanhoye@wolterskluwer.com>
2022-05-11 21:08:07 +0200

    Module2

---
+++ ssg/parsers.py
@@ -0,0 +1,12 @@
+from typing import List
+from pathlib import Path
+
+class Parser:
+    extensions: List[str] = []
+
+
+    def valid_extension(self, extension):
+        return (extension in self.extensions)
+
+    def parse(self, path: Path, source: Path, dest: Path)
+        pass

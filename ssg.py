8efad7a8b052448d0379cf7107c307b422b0a019
Johan Van Hoye <johan.vanhoye@wolterskluwer.com>
2022-05-11 21:47:57 +0200

    Module2

--- ssg.py
+++ ssg.py
@@ -1,9 +1,10 @@
+from ssg.parsers import *
 import typer
 from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config = { "source": source, "dest": dest}
+    config = { "source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
 
     Site(**config).build()
 
--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,5 +1,6 @@
 from typing import List
 from pathlib import Path
+import shutil
 
 class Parser:
     extensions: List[str] = []
@@ -8,5 +9,24 @@ class Parser:
     def valid_extension(self, extension):
         return (extension in self.extensions)
 
-    def parse(self, path: Path, source: Path, dest: Path)
-        pass
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with path.open(mode='r') as file:
+            return file.read_text()
+
+    def write(self, path, dest, content, ext = ".html"):
+        full_path = self.dest / path.with_suffix(ext).name
+        with full_path.open(mode='w') as file':
+            file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        Parser.copy(path, source, dest)
